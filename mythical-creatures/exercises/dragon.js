c46cfa3ebd67020348287fb077b0d6ecc06eff8b
Zach Stearns <zach.stearns92@gmail.com>
2021-04-29 21:14:18 -0600

    Complete Dragon

--- mythical-creatures/exercises/dragon.js
+++ mythical-creatures/exercises/dragon.js
@@ -1,6 +1,22 @@
 
 class Dragon {
+  constructor(name, rider) {
+    this.name = name;
+    this.rider = rider;
+    this.mealCount = 0;
+    this.hungry = true;
+  }
 
+  greet() {
+    return `Hi, ${this.rider}!`
+  }
+
+  eat() {
+    this.mealCount += 1;
+    if (this.mealCount >= 3) {
+      this.hungry = false;
+    }
+  }
 }
 
 module.exports = Dragon;
